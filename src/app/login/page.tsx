d67171fd2abbdc25c220ecee89cb31f1f2309d5a
hexaalif <shadullahsad9@gmail.com>
2024-10-03 20:39:22 +0600

    content-type , stringify added 7

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,16 +6,24 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store/store";
+import { login } from "@/store/Reducers/authSlice";
+// import { useRouter } from "next/";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState(null);
   const router = useRouter();
+  // const dispatch = useDispatch<AppDispatch>();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
     setError(null);
+    setSuccess(null);
+    console.log("Form submitted");
 
     const formData = new FormData(e.currentTarget);
     const email = formData.get("email");
@@ -32,31 +40,44 @@ const Login = () => {
         }
       );
 
-      const { accessToken, refreshToken, user } = res.data?.data;
+      console.log(email, password);
 
-      localStorage.setItem("id", user._id);
+      const { accessToken, refreshToken, user } = res.data?.data;
+      console.log(res.data?.data);
+      const { _id } = user;
+      console.log(_id);
+
+      localStorage.setItem("id", _id);
       localStorage.setItem("accessToken", accessToken);
       localStorage.setItem("refreshToken", refreshToken);
 
+      // dispatch(
+      //   login({
+      //     email: formData.get("email") as string,
+      //     password: formData.get("password") as string,
+      //   })
+      // );
+
       router.push("/");
     } catch (error: any) {
-      if (error.response?.data?.message) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
         setError(error.response.data.message);
       } else {
-        setError("An error occurred during login. Please try again.");
+        setError("An Error occurred during Login. Please try again");
       }
-    } finally {
-      setLoading(false); // Ensure loading is set to false after the request
     }
   };
-
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200 mt-28">
         Welcome to Login Page
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Login to unlock the features of lazz-pharma
+        Login to Unlock the features of lazz-pharma
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
@@ -84,16 +105,17 @@ const Login = () => {
         {error && <p className="text-red-500">{error}</p>}
 
         <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium"
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          {loading ? "Logging in..." : "Login"} &rarr;
+          {loading ? "loggin..." : `Login`} &rarr;
+          <BottomGradient />
         </button>
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
       </form>
       <p>
-        Create an Account?{" "}
+        Create An Account?{" "}
         <span className="italic underline">
           <Link href={"/register"}>Register Here</Link>
         </span>
@@ -102,12 +124,27 @@ const Login = () => {
   );
 };
 
-const LabelInputContainer = ({ children, className }) => {
+export default Login;
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
     <div className={cn("flex flex-col space-y-2 w-full", className)}>
       {children}
     </div>
   );
 };
-
-export default Login;

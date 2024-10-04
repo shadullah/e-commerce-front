8bb17c1102d7ceac39646927d06f4dcf95bd7c71
hexaalif <shadullahsad9@gmail.com>
2024-10-05 00:29:58 +0600

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,19 @@
-// "use client";
+"use client";
 
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// const getId = () => {
-//   if (typeof window === "undefined") {
-//     return "";
-//   }
-//   return localStorage.getItem("id");
-// };
+const getId = () => {
+  if (typeof window === "undefined") {
+    return "";
+  }
+  return localStorage.getItem("id");
+};
 
-// export const middleware = (req) => {
-//   if (!getId()) {
-//     return NextResponse.redirect(new URL("/login", req.url));
-//   }
-// };
+export const middleware = (req) => {
+  if (!getId()) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+};
 
 export const config = {
   matcher: ["/dashboard", "/order"],
